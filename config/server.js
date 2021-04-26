b60da78eb83a135ed89b41398b4a885980e59e70
Trystan VALENTIN <trystan.valentin@gmail.com>
2021-04-26 10:54:22 +0200

    création projet API avec strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'eed0c24ecc9d38107060b2125ad12f72'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '40408a6e-528c-48e3-853c-b3c0ba84024c'
+};
